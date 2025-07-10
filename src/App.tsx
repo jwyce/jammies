65e041f1543fcdb7dc5a70c550ca7e5e7cbef401
Jared Wyce <wycejared@gmail.com>
2025-07-10 07:45:21 -0400

    💃 more dance emotes

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import { Music, Music2 } from "lucide-react";
 export function App() {
   const emotes = [
     {
-      name: "jammies",
-      url: "https://cdn.betterttv.net/emote/5d2dc7dcff6ed3680130eb6d/3x.webp",
+      name: "pepeD",
+      url: "https://cdn.betterttv.net/emote/5b1740221c5a6065a7bad4b5/3x.webp",
     },
     {
       name: "catjam",
@@ -30,6 +30,50 @@ export function App() {
     {
       name: "squirtleJam",
       url: "https://cdn.betterttv.net/emote/6011c9536c75a765d463e478/3x.webp",
+    },
+    {
+      name: "VIBE",
+      url: "https://cdn.betterttv.net/emote/6228bfe606fd6a9f5be69c39/3x.webp",
+    },
+    {
+      name: "dekuHYPE",
+      url: "https://cdn.betterttv.net/emote/594c13b436b6a43b492ce4bd/3x.webp",
+    },
+    {
+      name: "xar2EDM",
+      url: "https://cdn.betterttv.net/emote/5b7e01fbe429f82909e0013a/3x.webp",
+    },
+    {
+      name: "rareChar",
+      url: "https://cdn.betterttv.net/emote/562b9101a6646e202bcc5447/3x.webp",
+    },
+    {
+      name: "KirbDance",
+      url: "https://cdn.betterttv.net/emote/5b9011eea2c5266ff2b8fde5/3x.webp",
+    },
+    {
+      name: "HAMDANCE",
+      url: "https://cdn.betterttv.net/emote/5b3aa96a05e329206af700c8/3x.webp",
+    },
+    {
+      name: "dogJAM",
+      url: "https://cdn.betterttv.net/emote/5f2e2fcf6f378244660275ae/3x.webp",
+    },
+    {
+      name: "CATJAM",
+      url: "https://cdn.betterttv.net/emote/60c5d2fdf8b3f62601c3d25b/3x.webp",
+    },
+    {
+      name: "PepoDance",
+      url: "https://cdn.betterttv.net/emote/5a6edb51f730010d194bdd46/3x.webp",
+    },
+    {
+      name: "BirbRave",
+      url: "https://cdn.betterttv.net/emote/5c32f7f9b2b08c560f6ae60a/3x.webp",
+    },
+    {
+      name: "crabRave",
+      url: "https://cdn.betterttv.net/emote/5dbaeed327360247dd64e03d/3x.webp",
     },
   ];
 
